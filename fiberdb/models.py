403d74f69014252c6cd606f21bbec7a11bf642ac
Michael Rudden <michael.rudden@gmail.com>
2013-01-07 11:07:03 -0500

    setting up models

--- fiberdb/models.py
+++ fiberdb/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+class Cable(models.Model):
+
+
+class Strand(models.Model):
+
+
+class Connector_Type(models.Model):
+
+
+class Adaptor_Plate_Connector(models.Model):
+
+
+class Adaptor_Plate(models.Model):
+
+
+class Box(models.Model):
+
+
+class Rack(models.Model):
+
+
+class Lan_Room(models.Model):
+
+
+class Building(models.Model):
+
+
+class Fiber_Type(models.Model):
+
+
+class Path(models.Model):
+
+
+class Point(models.Model):
+
+
+class Point_Type(models.Model):
