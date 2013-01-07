faa0746831e41389a2d713c4aecbed2b2ccd602c
Michael Rudden <michael.rudden@gmail.com>
2013-01-07 13:07:00 -0500

    defining models

--- fiberdb/models.py
+++ fiberdb/models.py
@@ -1,40 +1,59 @@
 from django.db import models
 
-# Create your models here.
 class Cable(models.Model):
-
+    fiber_type_id = models.ForeignKey('FiberType')
+    end1_box_id = models.ForeignKey('Box')
+    end2_box_id = models.ForeignKey('Box')
+    strands = models.CharField(max_length=200)
+    length = models.CharField(max_length=200)
+    date_added = models.DateTimeField('Date Added')
 
 class Strand(models.Model):
-
+    cable_id = models.ForeignKey('Cable')
+    end1_plate_connector_id = models.ForeignKey('AdaptorPlateConnector')
+    end2_plate_connector_id = models.ForeignKey('AdaptorPlateConnector')
+    in_use = models.CharField(max_length=200)
+    desc = models.CharField(max_length=200)
+    date_added = models.DateTimeField('Date Added')
 
 class Connector_Type(models.Model):
+    type = models.CharField(max_length=200)
 
+class AdaptorPlateConnector(models.Model):
+    adaptor_plate_id = models.ForeignKey('AdaptorPlate')
+    connector_type_id = models.ForeignKey('ConnectorType')
+    strand_position = models.CharField(max_length=200)
+    date_added = models.DateTimeField('Date Added')
 
-class Adaptor_Plate_Connector(models.Model):
-
-
-class Adaptor_Plate(models.Model):
-
+class AdaptorPlate(models.Model):
+    box_id = models.ForeignKey('Box')
+    box_position = models.CharField(max_length=200)
+    alignment = models.CharField(max_length=200)
+    date_added = models.DateTimeField('Date Added')
 
 class Box(models.Model):
-
+    rack_id = models.ForeignKey('Rack')
+    box_name = models.CharField(max_length=200)
+    date_added = models.DateTimeField('Date Added')
 
 class Rack(models.Model):
+    date_added = models.DateTimeField('Date Added')
 
-
-class Lan_Room(models.Model):
-
+class LanRoom(models.Model):
+    date_added = models.DateTimeField('Date Added')
 
 class Building(models.Model):
+    date_added = models.DateTimeField('Date Added')
 
-
-class Fiber_Type(models.Model):
-
+class FiberType(models.Model):
+    fiber_type = models.CharField(max_length=200)
+    micron = models.CharField(max_length=200)
 
 class Path(models.Model):
-
+    date_added = models.DateTimeField('Date Added')
 
 class Point(models.Model):
+    date_added = models.DateTimeField('Date Added')
 
-
-class Point_Type(models.Model):
+class PointType(models.Model):
+    date_added = models.DateTimeField('Date Added')
