e4e8c2b973d104a6ca9fb0a5434196395aac1ef3
Michael Rudden <michael.rudden@gmail.com>
2013-01-17 15:52:47 -0500

    experimenting with views

--- fiberdb/views.py
+++ fiberdb/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
+from django.views.generic.list import ListView
+from fiberdb.models import *
 
-
+#class RackListView(ListView
+#   model = Rack
+#   
+#
+#
