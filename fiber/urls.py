d5fd3cb950ae6ed44ab71cd9885c69a5ac5cab40
Michael Rudden <michael.rudden@gmail.com>
2013-02-15 14:20:45 -0500

    disabling add pages for test deployment

--- fiber/urls.py
+++ fiber/urls.py
@@ -19,12 +19,12 @@ urlpatterns = patterns('',
         DetailView.as_view(
             model=Building,
             template_name='fiberdb/buildings_detail.html')),
-    url(r'^buildings/add/$',
-        CreateView.as_view(
-            form_class=AddBuilding,
-            model='Building',
-            success_url='/buildings/',
-            template_name='fiberdb/add_building.html')),
+#    url(r'^buildings/add/$',
+#        CreateView.as_view(
+#            form_class=AddBuilding,
+#            model='Building',
+#            success_url='/buildings/',
+#            template_name='fiberdb/add_building.html')),
     # LAN Rooms
     url(r'^lanrooms/$',
         ListView.as_view(
@@ -35,36 +35,40 @@ urlpatterns = patterns('',
         DetailView.as_view(
             model=LanRoom,
             template_name='fiberdb/lanrooms_detail.html')),
-    url(r'^lanrooms/add/$',
-        CreateView.as_view(
-            form_class=AddLanRoom,
-            model='LanRoom',
-            success_url='/lanrooms/',
-            template_name='fiberdb/add_lanroom.html')),
+#    url(r'^lanrooms/add/$',
+#        CreateView.as_view(
+#            form_class=AddLanRoom,
+#            model='LanRoom',
+#            success_url='/lanrooms/',
+#            template_name='fiberdb/add_lanroom.html')),
     # Racks
     url(r'^racks/$',
         ListView.as_view(
             queryset=Rack.objects,
             context_object_name="rack_list",
             template_name='fiberdb/racks.html')),
-    url(r'^racks/add/$',
-        CreateView.as_view(
-            form_class=AddRack,
-            model='Rack',
-            success_url='/racks/',
-            template_name='fiberdb/add_rack.html')),
+    url(r'^racks/(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=Rack,
+            template_name='fiberdb/lanrooms_detail.html')),
+#    url(r'^racks/add/$',
+#        CreateView.as_view(
+#            form_class=AddRack,
+#            model='Rack',
+#            success_url='/racks/',
+#            template_name='fiberdb/add_rack.html')),
     # Boxes
     url(r'^boxes/$',
         ListView.as_view(
             queryset=Box.objects,
             context_object_name="box_list",
             template_name='fiberdb/boxes.html')),
-    url(r'^boxes/add/$',
-        CreateView.as_view(
-            form_class=AddBox,
-            model='Box',
-            success_url='/boxes/',
-            template_name='fiberdb/add_box.html')),
+#    url(r'^boxes/add/$',
+#        CreateView.as_view(
+#            form_class=AddBox,
+#            model='Box',
+#            success_url='/boxes/',
+#            template_name='fiberdb/add_box.html')),
 )
 
 urlpatterns += patterns('', 
