a66857556466669a9087652deac435842ae79b97
Michael Rudden <michael.rudden@gmail.com>
2013-03-08 16:10:33 -0500

    updated forms.py

--- fiberdb/forms.py
+++ fiberdb/forms.py
@@ -19,3 +19,39 @@ class AddRack(ModelForm):
 class AddBox(ModelForm):
     class Meta:
         model = Box
+
+class AddAdaptorPlate(ModelForm):
+    class Meta:
+        model = AdaptorPlate
+
+class AddAdaptorPlateConnector(ModelForm):
+    class Meta:
+        model = AdaptorPlateConnector
+
+class AddConnectorType(ModelForm):
+    class Meta:
+        model = ConnectorType
+
+class AddCable(ModelForm):
+    class Meta:
+        model = Cable
+
+class AddStrand(ModelForm):
+    class Meta:
+        model = Strand
+
+class AddFiberType(ModelForm):
+    class Meta:
+        model = FiberType
+
+class AddPath(ModelForm):
+    class Meta:
+        model = Path
+
+class AddPoint(ModelForm):
+    class Meta:
+        model = Path
+
+class AddPointType(ModelForm):
+    class Meta:
+        model = Path
